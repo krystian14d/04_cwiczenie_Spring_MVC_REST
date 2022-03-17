92c695690daa486d9fb7563ee1e3ac8a724fc7ce
Krystian <krystian14d@gmail.com>
2022-03-17 08:25:37 +0100

    added user edit feature

--- src/main/java/pl/javastart/equipy/user/UserResource.java
+++ src/main/java/pl/javastart/equipy/user/UserResource.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,8 +33,8 @@ public class UserResource {
     }
 
     @PostMapping("")
-    public ResponseEntity<UserDto> save(@RequestBody UserDto user){
-        if(user.getId() != null)
+    public ResponseEntity<UserDto> save(@RequestBody UserDto user) {
+        if (user.getId() != null)
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Zapisywany obiekt nie może mieć ustawionego id");
         UserDto savedUser = userService.save(user);
         URI location = ServletUriComponentsBuilder
@@ -41,6 +43,22 @@ public class UserResource {
                 .buildAndExpand(savedUser.getId())
                 .toUri();
         return ResponseEntity.created(location).body(savedUser);
-
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> findById(@PathVariable Long id) {
+        return userService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> update(@PathVariable Long id, @RequestBody UserDto user) {
+        if (!id.equals(user.getId())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Aktualizowany obiekt musi mieć ID zgodne z ID w ścieżce zasobu");
+        }
+        UserDto updatedUser = userService.update(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
 }
--- src/main/java/pl/javastart/equipy/user/UserService.java
+++ src/main/java/pl/javastart/equipy/user/UserService.java
@@ -32,8 +32,24 @@ public class UserService {
         userByPesel.ifPresent(u -> {
             throw new DuplicatePeselException();
         });
+        return mapAndSaveUser(user);
+    }
+
+    UserDto update(UserDto user){
+        Optional<User> userByPesel = userRepository.findByPesel(user.getPesel());
+        userByPesel.ifPresent(u -> {
+            throw new DuplicatePeselException();
+        });
+        return mapAndSaveUser(user);
+    }
+
+    private UserDto mapAndSaveUser(UserDto user) {
         User userEntity = UserMapper.toEntity(user);
         User savedUser = userRepository.save(userEntity);
         return UserMapper.toDto(savedUser);
+    }
+
+    Optional<UserDto> findById(Long id){
+        return userRepository.findById(id).map(UserMapper::toDto);
     }
 }
